d1fdead3bb361008b60faa5cf014bd79f46b4a94
최재원 <46291115+choijaewon323@users.noreply.github.com>
2025-05-04 23:05:41 +0900

    feat: 새 게시글 구현

--- src/Components/NewBoard.tsx
+++ src/Components/NewBoard.tsx
@@ -1,9 +1,22 @@
-import {Link} from "react-router";
+import {Link, useNavigate} from "react-router";
+import {useAuth} from "../AuthContext.tsx";
 
 export default function NewBoard() {
+    const navigate = useNavigate();
+    const {user} = useAuth();
+
+    function onClick() {
+        if (user === null) {
+            alert("로그인이 필요합니다");
+            navigate("/");
+            return;
+        }
+        navigate("/board/new")
+    }
+
     return <>
         <div>
-            <Link to={"/board/new"}>새 게시글 작성</Link>
+            <button onClick={onClick}>새 게시글 작성</button>
         </div>
     </>
 }
--- src/routes/pages/NewBoardPage.tsx
+++ src/routes/pages/NewBoardPage.tsx
@@ -1,7 +1,54 @@
+import * as React from "react";
+import {useAuth} from "../../AuthContext.tsx";
+import {useNavigate} from "react-router";
+import DOMAIN from "../Domain.tsx";
+
+interface RequestBody {
+    title: string;
+    content: string;
+    writer: string;
+    category: string;
+}
 
 export default function NewBoardPage() {
+    const [title, setTitle] = React.useState<string>("");
+    const [content,  setContent] = React.useState<string>("");
+    const [category,  setCategory] = React.useState<string>("");
+    const {user} = useAuth();
+    const navigate = useNavigate();
 
+    async function onClick() {
+        const requestBody: RequestBody = {
+            title: title,
+            content: content,
+            writer: user,
+            category: category
+        }
+
+        const response = await fetch(`${DOMAIN}/api/v1/board`, {
+            method: "POST",
+            body: JSON.stringify(requestBody),
+            headers: { "Content-Type": "application/json" }
+        });
+
+        const result: boolean = await response.json();
+
+        if (!result) {
+            throw new Error("게시글 추가에 실패했습니다");
+        }
+
+        navigate("/");
+    }
 
     return <>
+        <div>
+            <p><input placeholder={"카테고리를 입력하세요"} onChange={e => setCategory(e.target.value)} /></p>
+            <p><input placeholder={"제목을 입력하세요"} onChange={e => setTitle(e.target.value)}/></p>
+            <div>
+                <textarea placeholder={"내용을 입력하세요"} onChange={e => setContent(e.target.value)}></textarea>
+            </div>
+            <p>{user}</p>
+            <button onClick={onClick}>글 작성</button>
+        </div>
     </>
 }
